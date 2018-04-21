7ff9eec3bc21561101084e13e13f0127fa81729d
Shinya Yamaoka <syamaoka@mail.libmacro.com>
2018-04-21 20:02:25 +0900

    disable logging by OUnit

--- src/typeSafeConfig.ml
+++ src/typeSafeConfig.ml
@@ -20,22 +20,26 @@ let create () = Root
 let parse_path_fragments path = String.split_on_char '.' path
 
 let rec get_value_opt t fragments f =
-  let compare_fragment p1 p2 = (String.compare p1 p2) = 0 in
-  match fragments with
-  | [] -> None
-  | p1 :: rest -> begin
-      match t with
-      | Leaf(p2, v) when compare_fragment p1 p2 -> f v
-      | Branch(p2, nodes) when compare_fragment p1 p2 ->
-        begin
-          (* TODO *)
-          match List.map (fun n -> get_value_opt n rest f)  nodes with
-          | [] -> None
-          | hd :: _ -> hd
-        end
-      | _ -> None
-    end
-
+  let rec compare_fragment p1 p2 = (String.compare p1 p2) = 0 in
+  let rec find_first nodes frags =
+    match nodes with
+    | [] -> None
+    | t1 :: rest -> begin
+        match get_value t1 frags with
+        | Some x -> Some x
+        | None -> find_first rest frags
+      end
+  and get_value tree frags =
+    match frags with
+    | [] -> None
+    | p1 :: rest -> begin
+        match tree with
+        | Leaf(p2, v) when compare_fragment p1 p2 -> f v
+        | Branch(p2, nodes) when compare_fragment p1 p2 -> find_first nodes rest
+        | _ -> None
+      end
+  in get_value t fragments
+       
 let get_string_opt t path =
   get_value_opt t (parse_path_fragments path) (function
     | HoconString x -> Some x
@@ -52,6 +56,17 @@ let get_int_opt t path =
     | HoconInt x -> Some x
     | _ -> None
   )
+
+let get_string_list_opt t path =
+  get_value_opt t (parse_path_fragments path) (function
+    | HoconStringList x -> Some x
+    | _ -> None
+  )
+
+let get_string_list t path =
+  match get_string_list_opt t path with
+  | Some x -> x
+  | None -> raise (ConfigMissing (Printf.sprintf "%s missing" path))
 
 let get_int t path =
   match get_int_opt t path with
--- test/main.ml
+++ test/main.ml
@@ -1,8 +1,64 @@
 open TypeSafeConfig
+open OUnit2
 
-let () =
-  let config = Leaf("name", (HoconString "hoge")) in
-  begin
-    print_string (get_string config "name");
-    print_newline ();
-  end
+type person = {
+  p_name   : string;
+  p_age    : int;
+  p_emails : string list;
+}
+
+type server = {
+  s_ports : int list;
+}
+
+let person_to_config p = Branch(
+  "person",
+  [
+    Leaf("name", (HoconString p.p_name));
+    Leaf("age", (HoconInt p.p_age));
+    Leaf("emails", (HoconStringList p.p_emails));
+  ]
+)
+
+let server_to_config s = Branch(
+  "server",
+  [
+    Leaf("ports", (HoconIntList s.s_ports));
+  ]
+)
+
+let person1 = {
+  p_name = "john"; p_age = 30;
+  p_emails = ["a@example.com"; "b@example.com";]
+}
+
+let server1 = { s_ports = [80; 8080;]; }
+
+let compare_list l1 l2 = List.for_all2 (=) l1 l2
+
+let test_get_string _ =
+  let config = person_to_config person1 in
+  assert_equal person1.p_name (get_string config "person.name")
+    
+let test_get_int _ =
+  let config = person_to_config person1 in
+  assert_equal person1.p_age (get_int config "person.age")
+
+let test_get_string_list _ =
+  let config = person_to_config person1 in
+  assert_equal ~cmp:compare_list
+    person1.p_emails
+    (get_string_list config "person.emails")
+    
+let test_get_int_list _ =
+  let config = server_to_config server1 in
+  assert_equal server1.s_ports (get_int_list config "server.ports")
+
+let suite = "suite" >::: [
+  "test_get_string" >:: test_get_string;
+  "test_get_int" >:: test_get_int;
+  "test_get_string_list" >:: test_get_string_list;
+  "test_get_int_list" >:: test_get_int_list;
+]
+
+let () = OUnit2.run_test_tt_main suite
